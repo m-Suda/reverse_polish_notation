5551b826c6505c0326a8f999b0f7ff5772e640a9
masak <oicwam.orz1228@gmail.com>
2019-08-28 18:16:41 +0900

    余計な処理を短縮化

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,12 @@ import { NumberOfOperationCharacter } from './class/NumberOfOperationCharacter';
 import { Stack } from './class/Stack';
 import { Dist } from './class/Dist';
 
-const dist: Dist = new Dist([]);
-const stack: Stack = new Stack([]);
-
 const formula: string = "a + b + c * d + e / f";
 const formulaArr: string[] = [...formula.replace(/\s/g, '')];
 const FORMULA_LENGTH: number = formulaArr.length;
+
+const dist: Dist = new Dist([]);
+const stack: Stack = new Stack([]);
 
 for (let i = 0; i < FORMULA_LENGTH; i++) {
 
@@ -21,8 +21,7 @@ for (let i = 0; i < FORMULA_LENGTH; i++) {
         // 演算子がスタックトップよりも優先度が高ければ、スタックの中身を全て降ろし、その後スタックに積む。
         if (char.hasHigherPriorityThan(stack.top())) {
             while (!stack.isEmpty()) {
-                const stackTop = stack.pop();
-                dist.add(stackTop);
+                dist.add(stack.pop());
             }
         }
         stack.push(char.value);
@@ -32,9 +31,9 @@ for (let i = 0; i < FORMULA_LENGTH; i++) {
     dist.add(char.value);
 }
 
+// 最後にスタックに積まれた演算子を全て降ろす
 while (!stack.isEmpty()) {
-    const stackTop = stack.pop();
-    dist.add(stackTop);
+    dist.add(stack.pop());
 }
 
 const result = dist.list.join('');
